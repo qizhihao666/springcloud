d0e5a5ff601d3f5150d97881ef0d9748264cafe1
qizhihao666 <769924533@qq.com>
2021-02-24 14:14:47 +0800

    新建商户服务  -齐志豪

---
+++ spring-cloud-merchant/src/main/java/spring/cloud/merchant/MerchantApplication.java
@@ -0,0 +1,15 @@
+package spring.cloud.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MerchantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantApplication.class, args);
+    }
+
+}
